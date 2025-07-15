06b62d8a58f51551050896ae8f0be8f090c2fd9f
Lora-Yagnasree <yagnasreekella20@gmail.com>
2025-07-15 15:03:49 +0530

    db

--- project/settings.py
+++ project/settings.py
@@ -137,14 +137,11 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hrms',
-        'USER': 'myhrms',
-        'PASSWORD': 'hrms',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Database
 '''DATABASES = {
