ccaa8e63ab76d3fb5c3ee08958e4a819908a1915
Lora-BabyPrabha <ranambabyprabha@gmail.com>
2025-07-17 15:34:59 +0530

    change

--- app/views.py
+++ app/views.py
@@ -374,8 +374,7 @@ def employee_requests(request):
         'loan_requests': loan_requests,
         'time_entries': time_entries,
         'notifications': notifications,
-    })
-
+    })   
 
 #------------------------------------------------------------- Mark as read -- Notifications  #
 
