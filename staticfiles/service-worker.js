3cdb832e542401516ca23eff9fe3fe442266eeea
Lora-Yagnasree <yagnasreekella20@gmail.com>
2025-08-18 12:40:12 +0530

    ok

--- staticfiles/service-worker.js
+++ staticfiles/service-worker.js
@@ -1,5 +1,5 @@
-CACHE_NAME = 'aihr4u-cache-v1';
-const urlsToCache = ['/', '/static/css/base.css','/static/css/index.css','/static/css/dashboard.css','/static/js/app.js'];
+const CACHE_NAME = 'aihr4u-cache-v1';
+const urlsToCache = ['/', '/static/css/base.css','/static/css/index.css','/static/css/dashboard.css'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
